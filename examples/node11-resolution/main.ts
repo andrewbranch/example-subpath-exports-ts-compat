d8ad6f38f5edbf53b4b56e0c69d2d1d05819e704
Andrew Branch <andrew@wheream.io>
2022-09-20 15:45:38 -0700

    Initial commit

---
+++ examples/node11-resolution/main.ts
@@ -0,0 +1,22 @@
+import assert from "assert";
+import extensionless from "extensionless";
+import extensionlessOne from "extensionless/one";
+import extensionlessTwo from "extensionless/two";
+assert(extensionless === "extensionless/index.js");
+assert(extensionlessOne === "extensionless/one.js");
+assert(extensionlessTwo === "extensionless/two/index.js");
+
+import { index as pjr } from "package-json-redirects";
+import { one as pjrOne } from "package-json-redirects/one";
+import { two as pjrTwo } from "package-json-redirects/two";
+assert(pjr === "package-json-redirects/cjs/index.js");
+assert(pjrOne === "package-json-redirects/cjs/one.js");
+assert(pjrTwo === "package-json-redirects/cjs/two.js");
+
+// These fail in Node 11 because only TypeScript follows `typesVersions`
+import tvw from "types-versions-wildcards";
+import tvwOne from "types-versions-wildcards/one";
+import tvwTwo from "types-versions-wildcards/two";
+assert(tvw === "types-versions-wildcards/dist/index.js");
+assert(tvwOne === "types-versions-wildcards/dist/one.js");
+assert(tvwTwo === "types-versions-wildcards/dist/two.js");
---
+++ examples/node16-resolution/main.mts
@@ -0,0 +1,23 @@
+import assert from "assert";
+import extensionless from "extensionless";
+import extensionlessOne from "extensionless/one";
+import extensionlessTwo from "extensionless/two";
+assert(extensionless === "extensionless/index.mjs");
+assert(extensionlessOne === "extensionless/one.mjs");
+assert(extensionlessTwo === "extensionless/two/index.mjs");
+
+import { index as pjr } from "package-json-redirects";
+import { one as pjrOne } from "package-json-redirects/one";
+import { two as pjrTwo } from "package-json-redirects/two";
+assert(pjr === "package-json-redirects/esm/index.js");
+assert(pjrOne === "package-json-redirects/esm/one.js");
+assert(pjrTwo === "package-json-redirects/esm/two.js");
+
+import tvw from "types-versions-wildcards";
+import tvwOne from "types-versions-wildcards/one";
+import tvwTwo from "types-versions-wildcards/two";
+assert(tvw === "types-versions-wildcards/dist/index.js");
+assert(tvwOne === "types-versions-wildcards/dist/one.js");
+assert(tvwTwo === "types-versions-wildcards/dist/two.js");
+
+import "./main.cjs";
